b303f9d86c7d8ba081ce0489e638ee9cad05a8b6
Al B <al@Als-MacBook-Pro-6.local>
2024-08-08 11:55:57 +0100

    Disable purgecss which strips tailwind files

--- themes/my-saas/postcss.config.js
+++ themes/my-saas/postcss.config.js
@@ -11,6 +11,6 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ purgecss ] : [])
+    // ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ purgecss ] : [])
   ]
 }
