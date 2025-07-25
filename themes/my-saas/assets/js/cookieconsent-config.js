f491c85b87a0f59ae39317dcdbfb4a3f11cc756f
Al B <al@Als-MacBook-Pro-6.local>
2025-07-25 16:23:34 +0100

    Cookie consent banner

---
+++ themes/my-saas/assets/js/cookieconsent-config.js
@@ -0,0 +1,68 @@
+import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.1.0/dist/cookieconsent.umd.js';
+
+CookieConsent.run({
+    guiOptions: {
+        consentModal: {
+            layout: "bar",
+            position: "bottom",
+            equalWeightButtons: false,
+            flipButtons: false
+        },
+        preferencesModal: {
+            layout: "box",
+            position: "right",
+            equalWeightButtons: false,
+            flipButtons: false
+        }
+    },
+    categories: {
+        necessary: {
+            readOnly: true
+        },
+        analytics: {}
+    },
+    language: {
+        default: "en",
+        autoDetect: "browser",
+        translations: {
+            en: {
+                consentModal: {
+                    title: "We use cookies",
+                    description: "Our website uses essential cookies for its basic functionality and analytics cookies to understand how visitors interact with our site. By clicking 'Accept all', you consent to the use of both necessary and analytics cookies. You can manage your preferences or decline analytics cookies by clicking 'Manage preferences' or 'Reject all'.",
+                    acceptAllBtn: "Accept all",
+                    acceptNecessaryBtn: "Reject all",
+                    showPreferencesBtn: "Manage preferences",
+                    footer: "<a href=\"https://app.authorless.ai/pages/legals/privacy-policy/\">Privacy Policy</a>\n<a href=\"https://app.authorless.ai/pages/legals/terms/\">Terms and conditions</a>"
+                },
+                preferencesModal: {
+                    title: "Consent Preferences Center",
+                    acceptAllBtn: "Accept all",
+                    acceptNecessaryBtn: "Reject all",
+                    savePreferencesBtn: "Save preferences",
+                    closeIconLabel: "Close modal",
+                    serviceCounterLabel: "Service|Services",
+                    sections: [
+                        {
+                            title: "Your Privacy",
+                            description: "When you visit our website, it may store or retrieve information on your browser, mostly in the form of cookies. This information might be about you, your preferences or your device and is mostly used to make the site work as you expect it to. The information does not usually directly identify you, but it can give you a more personalized web experience. Because we respect your right to privacy, you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. However, blocking some types of cookies may impact your experience of the site and the services we are able to offer."
+                        },
+                        {
+                            title: "Strictly Necessary Cookies <span class=\"pm__badge\">Always Enabled</span>",
+                            description: "These cookies are essential for the website to function properly and cannot be switched off in our systems. They are usually only set in response to actions made by you which amount to a request for services, such as setting your privacy preferences, logging in, or filling in forms. You can set your browser to block or alert you about these cookies, but some parts of the site will not then work. These cookies do not store any personally identifiable information.",
+                            linkedCategory: "necessary"
+                        },
+                        {
+                            title: "Analytics Cookies",
+                            description: "These cookies allow us to count visits and traffic sources so we can measure and improve the performance of our site. They help us to know which pages are the most and least popular and see how visitors move around the site. All information these cookies collect is aggregated and therefore anonymous. If you do not allow these cookies we will not know when you have visited our site, and will not be able to monitor its performance.",
+                            linkedCategory: "analytics"
+                        },
+                        {
+                            title: "More information",
+                            description: "For any query in relation to our policy on cookies and your choices, please visit our <a class=\"cc__link\" href=\"https://app.authorless.ai/pages/legals/privacy-policy/\">Privacy Policy</a> or <a class=\"cc__link\" href=\"https://app.authorless.ai/pages/legals/terms/\">Terms and Conditions</a> page."
+                        }
+                    ]
+                }
+            }
+        }
+    }
+});
