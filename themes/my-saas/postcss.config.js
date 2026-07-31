e7953ac2efdba8856ee07633ad15ff2ca9e7d97b
Al <al@example.com>
2026-07-31 14:23:45 +0100

    Just build with the latest hugo in the GHA

--- themes/my-saas/postcss.config.js
+++ themes/my-saas/postcss.config.js
@@ -1,16 +1,5 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./hugo_stats.json"],
-  defaultExtractor: (content) => {
-    const els = JSON.parse(content).htmlElements;
-    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
-  },
-  safelist: [],
-});
-
-
 module.exports = {
   plugins: {
     "@tailwindcss/postcss": {},
-    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
   }
 }
